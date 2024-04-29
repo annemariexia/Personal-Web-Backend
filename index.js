59b3defe38b9fb993bf9018ca4dc9c4e9753d7d6
Annemarie Xia <114361428+annemariexia@users.noreply.github.com>
2024-04-28 23:11:47 -0400

    update server

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const functions = require("firebase-functions");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -25,17 +5,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 
-// import express from "express";
-// import bodyParser from "body-parser";
-// import cors from "cors";
-// import nodemailer from "nodemailer";
-// import functions from "firebase-functions";
-// import dotenv from "dotenv";
-
 dotenv.config();
-
-// const CONTACT_EMAIL = process.env.VITE_CONTACT_EMAIL;
-// const EMAIL_PASSWORD = process.env.VITE_CONTACT_PSSW;
 
 const CONTACT_EMAIL = functions.config().env.vite_contact_email;
 const EMAIL_PASSWORD = functions.config().env.vite_contact_pssw;
